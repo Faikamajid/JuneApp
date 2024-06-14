9c6661053ec9ca488eb46c2a39d164ea50ae7015
faika majid <faikamajeed11@gmail.com>
2024-06-14 16:49:20 +0530

    first commit w11

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          My first React app Demo
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
